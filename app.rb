78f148f3e9f7a451c4579f78a767dad154083c7b
dagpan <panagiotisntagkinis@gmail.com>
2020-02-20 16:58:07 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,41 @@
 require './environment'
+require_relative './app/models/pirate.rb'
+require_relative './app/models/ship.rb'
+
 
 module FormsLab
   class App < Sinatra::Base
-
+      set :views, "./views/pirates/"
     # code other routes/actions here
+      get '/' do
+         "Welcome to the Nested Forms Lab!"
+      end
+
+      get '/new' do
+   #      binding.pry
+         erb :new
+      end
+
+      post '/pirates' do
+   #      binding.pry
+          hash1 = {}  
+          hash1[:name] = params[:pirate][:name]
+          hash1[:weight] = params[:pirate][:weight]
+          hash1[:height] = params[:pirate][:height]
+           @pirate = Pirate.new(hash1)
+  #       binding.pry  
+           params[:pirate][:ships].each do |details|
+   #              binding.pry
+                 Ship.new(details)
+           end
+           @ships = Ship.all
+           erb :show
+      end
 
   end
 end
+
+
+
+
+# 2. Make a nested form (this should probably have html `<label>`s so it makes sense to a user). This form will be creating three objects (one instance of the Pirate class, and two instances of the Ship class). Remember, you'll need to build a corresponding controller action to load this page. Your form should `POST` to the route `'/pirates'`.
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,25 @@
+
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+
+    @@all = []
+
+
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
 end
+
+# 1. Create two classes, a `Pirate` class and a `Ship` class. Pirates should have a name, weight, and height. You will also need a class method that returns all the pirates. The ship class should have name, type, and booty attributes, as well as a class method `.all` that returns all the ships and a class method `.clear` that deletes all ships.
